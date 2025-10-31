b94964cd7507e63d58e03258a479cc7bced4b848
Kostiantyn <firstpickpudge@gmail.com>
2025-10-31 10:05:15 +0200

    added mongo connection log

--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -2,10 +2,15 @@ const fp = require('fastify-plugin');
 const mongoose = require('mongoose');
 
 module.exports = fp(async function (fastify, opts) {
-  await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  fastify.decorate('mongoose', mongoose);
-  fastify.log.info('MongoDB connected');
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    fastify.decorate('mongoose', mongoose);
+    fastify.log.info('MongoDB connected');
+  } catch (err) {
+    fastify.log.error('MongoDB connection error:', err);
+    throw err;
+  }
 });
